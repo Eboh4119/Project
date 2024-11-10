62f47c2fd62c268cf7af2964d6025ed00cca7062
Eboh Benjamin <156227731+Eboh4119@users.noreply.github.com>
2024-11-10 00:01:20 +0000

    meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,8 @@ export default function RootLayout({
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9259829903766831"
      crossOrigin="anonymous"></script>
+     <meta name="google-adsense-account" content="ca-pub-9259829903766831"></meta>
       </Head>
-      <meta name="google-adsense-account" content="ca-pub-9259829903766831"></meta>
       <body className={inter.className}>{children}</body>
     </html>
   );
